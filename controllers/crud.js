5a31449a9b08aa1596980ffef2970a59814d8a8f
Yuseydi <pamelacamisan26@gmail.com>
2024-01-22 13:47:59 -0500

    tareas commit

--- controllers/crud.js
+++ controllers/crud.js
@@ -17,3 +17,35 @@ if (!nombre || !fechaContratacion || !salario || !horasTrabajadas || !departamen
     res.redirect('/registrar')
     })
 }
+exports.tareas = (req, res) => {
+    const { id_empleado, horas } = req.body;
+
+     
+
+    conexion.query( 'SELECT horasTrabajadas FROM empleados WHERE id = ?', [id_empleado], (err, resultadosEmpleado) => {
+        if (err) {
+            console.log(err);
+        }
+
+        const horasTrabajadas = resultadosEmpleado[0].horasTrabajadas;
+
+        if (horasTrabajadas < horas) {
+          const mensaje=   'error horas de tarea mayores a las de trabajadas';
+            return res.render('tareasmensaje',{mensaje});
+        } else {
+            const newTarea = {
+                nombre: req.body.nombre,
+                horas: req.body.horas,
+                id_empleado: req.body.id_empleado
+            };
+
+            conexion.query('INSERT INTO tareas SET ?', newTarea, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+
+                return res.redirect('/');
+            });
+        }
+    });
+};
--- router.js
+++ router.js
@@ -30,4 +30,14 @@ router.get('/registrar', (req, res) => {
 const crud=require('./controllers/crud')
 router.post('/guardar',crud.save)
 
+router.get('/tareas',(req,res)=>{
+    conexion.query('SELECT * FROM empleados',(err,result)=>{
+        if(err)
+        throw err
+    else
+    res.render('tareasemp',{result:result})
+    })
+})
+router.post('/tareasguardar',crud.tareas)
+
 module.exports=router
